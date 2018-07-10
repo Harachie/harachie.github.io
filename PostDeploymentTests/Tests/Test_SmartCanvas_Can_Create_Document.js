9df7c795a878bcefa981bff10d81b0ae49ae8ed9
Bastian Kröger <bkr@efi.com>
2018-07-10 07:29:34 +0200

    added simple test runner added test loading from json file added html file to run tests

---
+++ PostDeploymentTests/Tests/Test_SmartCanvas_Can_Create_Document.js
@@ -0,0 +1,11 @@
+﻿QUnit.test("ok test", function (assert) {
+    assert.ok(true, "true succeeds");
+    assert.ok("non-empty", "non-empty string succeeds");
+
+    assert.ok(false, "false fails");
+    assert.ok(0, "0 fails");
+    assert.ok(NaN, "NaN fails");
+    assert.ok("", "empty string fails");
+    assert.ok(null, "null fails");
+    assert.ok(undefined, "undefined fails");
+});
