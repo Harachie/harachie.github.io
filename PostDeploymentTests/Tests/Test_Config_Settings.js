3fed41d5f8ca65c5d0074d262ab80ebf4158b99d
Bastian Kröger <bkr@efi.com>
2018-07-10 11:49:53 +0200

    added simple config check

---
+++ PostDeploymentTests/Tests/Test_Config_Settings.js
@@ -0,0 +1,4 @@
+﻿QUnit.test("Configuration checks", function (assert) {
+    assert.ok(config.targetSystem !== null, "TargetSystem is not null");
+    assert.ok(config.targetSystem !== "", "TargetSystem is not empty");
+});
