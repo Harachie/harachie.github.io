d7bcf719f9557b79e83d7ead7d1297f2d7e9428c
Bastian Kröger <bkr@efi.com>
2018-07-11 08:47:36 +0200

    added cleanUp method added phantomJS test script cleaned up phantomJS tests

--- PostDeploymentTests/RunTests.js
+++ PostDeploymentTests/RunTests.js
@@ -1,12 +1,46 @@
 ﻿var page = require('webpage').create();
-var url = "https://harachie.github.io/PostDeploymentTests/RunTests.html#{%22sourceSystem%22:%22harachie.github.io/PostDeploymentTests%22,%22targetSystem%22:%22xmg-bkr.efi.internal%22,%22testSuite%22:%22/Tests/SmartCanvasTests.json%22,%22publicKey%22:%2282E1F02F%22,%22privateKey%22:%224B8E980D67E5FDE0491A9724B90F5EAE9B642B26AC6E86B1%22,%22adminPublicKey%22:%22%22,%22adminPrivateKey%22:%22%22}";
+var args = require('system').args;
+var fs = require('fs');
+var url;
+
+var config = {
+    "sourceSystem": "xmg-bkr.efi.internal/PostDeploymentTests",
+    "targetSystem": "xmg-bkr.efi.internal",
+    "testSuite": "/Tests/SmartCanvasTests.json",
+    "publicKey": "82E1F02F",
+    "privateKey": "4B8E980D67E5FDE0491A9724B90F5EAE9B642B26AC6E86B1",
+    "adminPublicKey": "",
+    "adminPrivateKey": ""
+};
+
+if (args[1]) {
+    config = JSON.parse(decodeURI(args[1]));
+}
+
+url = "https://" + config.sourceSystem + "/RunTests.html#" + JSON.stringify(config);
+
+function writeFinalResult(data) {
+    console.log("Total: ", data.total, " Failed: ", data.failed, " Passed: ", data.passed, " Runtime: ", data.runtime);
+}
+
+function writeGenericErrorMessage(message) {
+    fs.write('output.html', message, 'w');
+    writeFinalResult({ "total": 1, "failed": 1, "passed": 0, "runtime": 1 });
+}
 
 page.open(url, function (status) {
-   
+    if (status !== "success") {
+        writeGenericErrorMessage("Failed to load url: " + url);
+        phantom.exit();
+    }
 });
 
-page.onCallback = function (data) {
-    console.log("Total: ", details.total, " Failed: ", details.failed, " Passed: ", details.passed, " Runtime: ", details.runtime);
+page.onCallback = function (data) {   
+    page.evaluate(function () {
+        cleanUp();
+    });
+    fs.write('output.html', page.content, 'w');
+    writeFinalResult(data);
     phantom.exit();
 };
 
